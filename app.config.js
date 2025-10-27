97f84b8f741fb44c53c4fb0597e1813b45dec085
MrGainn <109920512+MrGainn@users.noreply.github.com>
2025-10-27 19:04:14 +0100

    doen

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "GNSS_APP",
     slug: "GNSS_APP",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "dnssapp",
